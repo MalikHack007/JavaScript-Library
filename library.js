700ebc40bf98d73b81f72a486b546fa25d9676d4
MalikHack007 <Malikzhangggg@gmail.com>
2024-06-04 11:04:55 -0500

    Completed the delete button feature, read button feature will be added soon.

--- library.js
+++ library.js
@@ -42,7 +42,39 @@ bookForm.addEventListener('submit', (event)=>{
     let newBook = new book(bookNameInput.value, bookYearInput.value, authorInput.value, readStatus.value);
 
     library.push(newBook);
+    //Function to build the form out of the library
+    buildFormFromLibrary(library);
 
+    console.log(library);
+
+    newBookIntakeDialog.close();
+
+})
+
+
+function deleteButtonSetUp(event){
+    console.log('delete button clicked');
+    let parentTableRow = event.target.parentNode.parentNode;
+    let bookInfoStorage = [];
+    for(let i = 0; i < parentTableRow.childNodes.length - 1; i++){
+        bookInfoStorage.push(parentTableRow.childNodes[i].textContent);
+    }
+    let identifiedBook = new book(bookInfoStorage[0], bookInfoStorage[1], bookInfoStorage[2], bookInfoStorage[3]);
+    library.forEach((singleBook)=>{
+        if(singleBook.name == identifiedBook.name && singleBook.year == identifiedBook.year && singleBook.author == identifiedBook.author && singleBook.read == identifiedBook.read){
+            let index = library.indexOf(singleBook);
+            library.splice(index, 1);
+        } 
+    }) //remove that book from the library array
+
+    buildFormFromLibrary(library);
+    console.log('form built');
+}
+
+function buildFormFromLibrary(library){
+    let deleteButton;
+    let readButton;
+    bookTable.innerHTML = '';
     library.forEach((eachBook)=>{
         let newBookRow = document.createElement('tr');
         Object.values(eachBook).forEach((value)=>{
@@ -50,14 +82,19 @@ bookForm.addEventListener('submit', (event)=>{
             newBookData.textContent = value;
             newBookRow.appendChild(newBookData);
         })
+        readButton = document.createElement('button');
+        readButton.textContent = 'Read';
+        deleteButton = document.createElement('button');
+        deleteButton.textContent = 'Delete';
+        buttonCell = document.createElement('td');
+        buttonCell.appendChild(readButton);
+        buttonCell.appendChild(deleteButton);
+        newBookRow.appendChild(buttonCell);
         bookTable.appendChild(newBookRow);
+        deleteButton.addEventListener('click', deleteButtonSetUp);
     })
 
-    console.log(library);
-
-    newBookIntakeDialog.close();
-
-})
+}
 
 
 
